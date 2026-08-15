7bb62f66c0b7f9f3bf7e2400e401689264c44180
kmgeos <dansgkat@gmail.com>
2026-08-14 18:19:19 -0600

    Add configuration and initial group data

--- js/data.js
+++ js/data.js
@@ -1,1 +1,22 @@
+let config = {
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  longBreakInterval: 4,
+  darkModeRunning: true,
+  colorPomodoro: '#6b4c9a',
+  colorShort: '#2d8883',
+  colorLong: '#397097',
+  gifUrl: 'https://i.gifer.com/XOsX.gif'
+};
 
+let groups = [
+  {
+    id: 1,
+    name: "LABORAL PELICULA",
+    open: true,
+    tasks: [
+      { id: 101, text: "Ver escena 1", completed: false }
+    ]
+  }
+];
