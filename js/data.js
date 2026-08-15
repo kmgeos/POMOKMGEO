40201b804eee72a8ab9ff9dad89c862592b0ac67
kmgeos <dansgkat@gmail.com>
2026-08-15 14:55:58 -0600

    Enhance local storage handling for tasks and config
    Added functionality to store and retrieve groups and ungrouped tasks from local storage. Updated config structure to include reminder settings.

--- js/data.js
+++ js/data.js
@@ -1,22 +1,29 @@
-let config = {
+let groups = JSON.parse(localStorage.getItem('pomodoro_groups')) || [];
+let ungroupedTasks = JSON.parse(localStorage.getItem('ungrouped_tasks')) || [];
+
+let config = JSON.parse(localStorage.getItem('pomodoro_config')) || {
   pomodoro: 25,
   shortBreak: 5,
   longBreak: 15,
   longBreakInterval: 4,
+  autoBreaks: false,
+  autoPomos: false,
+  autoCheck: false,
+  checkBottom: true,
+  alarmSound: "kitchen",
+  alarmVol: 50,
+  alarmRepeat: 1,
   darkModeRunning: true,
-  colorPomodoro: '#6b4c9a',
-  colorShort: '#2d8883',
-  colorLong: '#397097',
-  gifUrl: 'https://i.gifer.com/XOsX.gif'
+  colorPomodoro: "#6b4c9a",
+  colorShort: "#2d8883",
+  colorLong: "#397097",
+  gifUrl: "https://i.gifer.com/XOsX.gif",
+  reminderType: "last",
+  reminderMin: 0
 };
 
-let groups = [
-  {
-    id: 1,
-    name: "LABORAL PELICULA",
-    open: true,
-    tasks: [
-      { id: 101, text: "Ver escena 1", completed: false }
-    ]
-  }
-];
+function saveData() {
+    localStorage.setItem('pomodoro_groups', JSON.stringify(groups));
+    localStorage.setItem('ungrouped_tasks', JSON.stringify(ungroupedTasks));
+    localStorage.setItem('pomodoro_config', JSON.stringify(config));
+}
